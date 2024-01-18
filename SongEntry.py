265d43c5c99f2f03e9a7ced8c1acc29f709adfe0
kznmrv <156931019+kznmrv@users.noreply.github.com>
2024-01-18 12:01:26 +0800

    Add files via upload

---
+++ SongEntry.py
@@ -0,0 +1,10 @@
+class SongEntry:
+    def __init__(self,
+                 id=1,
+                 title='Song Title',
+                 artist='Artist Name',
+                 album='Album Name'):
+        self.id = id
+        self.title = title
+        self.artist = artist
+        self.album = album
